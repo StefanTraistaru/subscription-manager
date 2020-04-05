247963c45d9097a6658535c86a0fca707a4d148e
Stefan Traistaru <stefan.traistaru.97@gmail.com>
2020-04-05 17:20:49 +0300

    Added material theme to table

--- web/src/app/components/subscription-list/subscription-list.component.ts
+++ web/src/app/components/subscription-list/subscription-list.component.ts
@@ -1,9 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { Subscription } from 'src/app/model/subscription'
 // import { subscriptions } from '../../subscriptions'
 
 import { WebService } from '../../web.service';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+const ELEMENT_DATA: Subscription[] = [
+  {name: 'Hydrogen', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+  {name: 'Helium', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+  {name: 'Lithium', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+  {name: 'Beryllium', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+  {name: 'Boron', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+  {name: 'Carbon', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+  {name: 'Nitrogen', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+  {name: 'Oxygen', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+  {name: 'Fluorine', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+  {name: 'Neon', price: '12', details: 'asd', date_d: 'a', date_m: 's', date_y: 'd'},
+];
+
 
 @Component({
   selector: 'app-subscription-list',
@@ -14,7 +31,14 @@ export class SubscriptionListComponent implements OnInit {
 
   // public my_subs;
   my_subs: Subscription[];
+  // displayedColumns: string[] = ['name', 'price', 'day', 'month', 'year'];
+  displayedColumns: string[] = ['name', 'price', 'details', 'date'];
+  expandedElement: Subscription | null;
 
+  dataSource = new MatTableDataSource<Subscription>(this.my_subs)
+  // dataSource = ELEMENT_DATA;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(
     public webService: WebService
@@ -29,6 +53,8 @@ export class SubscriptionListComponent implements OnInit {
     this.webService.getSubscriptions().subscribe(
       subs => {
         this.my_subs = subs;
+        this.dataSource = new MatTableDataSource<Subscription>(this.my_subs)
+        this.dataSource.paginator = this.paginator;
         console.log("Got subscriptions.");
       }
     )

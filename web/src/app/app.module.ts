ffacd67c9f0f7129af2718c521fb3433e41e685e
Stefan Traistaru <stefan.traistaru.97@gmail.com>
2020-04-05 16:37:14 +0300

    Added material theme compatibility to the web interface

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,13 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SubscriptionListComponent } from './components/subscription-list/subscription-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CreateSubscriptionComponent } from './components/create-subscription/create-subscription.component';
+// import 'hammerjs';
+// My imports
+import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { SubscriptionListComponent } from './components/subscription-list/subscription-list.component';
+import { CreateSubscriptionComponent } from './components/create-subscription/create-subscription.component';
+// Material theme imports
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+// import { MatButtonToggleModule } from '@angular/material/button-toggle';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatChipsModule } from '@angular/material/chips';
+// import { MatStepperModule } from '@angular/material/stepper';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatExpansionModule } from '@angular/material/expansion';
+// import { MatGridListModule } from '@angular/material/grid-list';
+// import { MatProgressBarModule } from '@angular/material/progress-bar';
+// import { MatRadioModule } from '@angular/material/radio';
+// import { MatRippleModule } from '@angular/material/core';
+// import { MatSliderModule } from '@angular/material/slider';
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// import { MatSortModule } from '@angular/material/sort';
+// import { MatTabsModule } from '@angular/material/tabs';
+// import { MatTooltipModule } from '@angular/material/tooltip';
+// import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   declarations: [
@@ -20,10 +55,45 @@ import { MainComponent } from './components/main/main.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    // Material theme imports
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    // MatOptionModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule
   ],
   exports: [
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    // MatOptionModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
